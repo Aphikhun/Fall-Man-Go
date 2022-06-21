713cbb5e00043d320d43ab598e73818d7f6580f0
Aphikhun <wi.aphikhun_st@tni.ac.th>
2022-06-21 14:18:06 +0700

    <add> game flow stage 2 <mod> map 2

---
+++ trigger_script/ScriptableNode_2.lua
@@ -0,0 +1,13 @@
+--You can use 'params.parameter name' to get the parameters defined in the node. 					
+--For example, if a parameter named 'entity' is defined in the node, you can use 'params.entity' to get the value of the parameter.
+local function has_value (tab, val)
+    for index, value in ipairs(tab) do
+        if value == val then
+            return true
+        end
+    end
+
+    return false
+end
+
+return has_value(params.listPlayer, params.player)
